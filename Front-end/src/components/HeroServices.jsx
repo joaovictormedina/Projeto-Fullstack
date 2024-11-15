558b1ca177d068fe65acfac23661f2ed94ff6409
João Victor Medina <joaovictor.zipp@gmail.com>
2024-11-15 16:13:44 -0300

    feat: adicione customizacao do nav sem os padroes

--- Front-end/src/components/HeroServices.jsx
+++ Front-end/src/components/HeroServices.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <header>
-      <h1>Services Welcome to Our Website!</h1>
+      <h1>Services Page Welcome to Our Website!</h1>
       <p>Explore our services and packages.</p>
     </header>
   );
